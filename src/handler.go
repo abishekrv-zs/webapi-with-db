e2b8ff2dd96c9c53be0e96d553b021b25cd88410
abishek-zs <abishek.rv@zopsmart.com>
2022-11-24 13:38:09 +0530

    implements query params for id, deptid

--- src/handler.go
+++ src/handler.go
@@ -30,8 +30,44 @@ func EmployeeHandler(w http.ResponseWriter, r *http.Request) {
 			allEmp = append(allEmp, emp)
 		}
 
+		var reqEmp []employee
+
+		switch queryPram := r.URL.Query(); true {
+
+		case queryPram.Has("id"):
+			for _, emp := range allEmp {
+				if emp.Id == queryPram.Get("id") {
+					reqEmp = append(reqEmp, emp)
+					break
+				}
+			}
+			if reqEmp == nil {
+				w.WriteHeader(http.StatusNotFound)
+				if _, err := w.Write([]byte(`{"error":"emp id not found"}`)); err != nil {
+					log.Println(err)
+					return
+				}
+			}
+
+		case queryPram.Has("deptid"):
+			for _, emp := range allEmp {
+				if emp.Dept.Id == queryPram.Get("deptid") {
+					reqEmp = append(reqEmp, emp)
+				}
+			}
+			if reqEmp == nil {
+				w.WriteHeader(http.StatusNotFound)
+				if _, err := w.Write([]byte(`{"error":"deptid not found"}`)); err != nil {
+					log.Println(err)
+				}
+			}
+
+		default:
+			reqEmp = allEmp
+		}
+
 		// Marshal the required employees and send as response
-		respBody, err := json.Marshal(allEmp)
+		respBody, err := json.Marshal(reqEmp)
 		if err != nil {
 			log.Println(err)
 			w.WriteHeader(http.StatusInternalServerError)
